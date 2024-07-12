c2830a4c24abff1d7bd450ad0fb927155ccb0cb9
ankursynez <ankursynez@gmail.com>
2024-07-12 22:27:04 +0530

    Updates

--- src/index.ts
+++ src/index.ts
@@ -5,14 +5,15 @@ export * from './errors/not-authenticate';
 export * from './errors/not-found-errors';
 export * from './errors/request-validation-error';
 
-
-export * from './middleware/current-user'
+export * from './middleware/current-user';
 export * from './middleware/error-handler';
-export * from './middleware/requireAuth'
-export * from './middleware/validate-request'
+export * from './middleware/requireAuth';
+export * from './middleware/validate-request';
 
 export * from './events/base-listener';
 export * from './events/base-publisher';
-export * from './events/subjects'
-export * from './events/ticket-created-events'
-export * from './events/ticket-updated-events'
+export * from './events/subjects';
+export * from './events/ticket-created-events';
+export * from './events/ticket-updated-events';
+
+export * from './events/types/order-status';
